00b62bfc800a446bfbbb12d2adc0b87adffdeaef
2oothpick <kobbyboateng14@gmail.com>
2024-01-03 07:09:12 +0000

    Add 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
-
+""" Scripts starts a Flask web app """
 from flask import Flask
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
-    return 'Hello HBNB!'
+@app.route("/")
+def hello_hbnb():
+    """ Returns HBNB """
+    return "Hello HBNB!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
