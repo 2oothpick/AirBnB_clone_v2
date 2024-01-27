ad240dc8f0ec0bab89c2d91ed6de8a4ea3d0bb15
2oothpick <kobbyboateng14@gmail.com>
2024-01-27 19:54:41 +0000

    Add 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,64 +1,59 @@
 #!/usr/bin/python3
 """
-Generate .tgz files
+Generate .tgz files and deploys it to 
+web servers
 """
-from os import path
-from datetime import datetime as dt
-from fabric.api import local, put, run, env
-from fabric.context_managers import lcd
 
+from time import strftime
+from fabric.api import *
+import os
 
+env.hosts = ['100.25.162.172', '54.152.165.14']
 env.user = "ubuntu"
-env.hosts = ["18.210.28.31", "3.236.121.230"]
 
 
 def do_pack():
-    """ generate .tgz file"""
-    archived = dt.utcnow()
-    if path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    p = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        archived.year, archived.month, archived.day,
-        archived.hour, archived.minute, archived.second)
-    if local("tar -cvzf {} web_static/".format(
-            p)).failed is True:
+    """Generate a .tgz file of web_static folder"""
+    try:
+        # create versions folder
+        local("mkdir -p versions")
+        # compress to versions folder
+        time = f"{strftime('%Y%m%d%H%M%S')}"
+        local(f"tar -cvzf versions/web_static_{time}.tgz web_static/")
+        # return filename
+        return f'verizon/web_static_{time}.tgz'
+    except:
         return None
-    return p
 
 
 def do_deploy(archive_path):
-    """ Deploy files to servers"""
-    print("archive_path")
-    if path.isfile("{}".format(archive_path)) is False:
-        return False
-    result = put("{}".format(
-        archive_path), "/tmp/{}".format(
-            archive_path.split("/")[1]))
-    if result.failed is True:
-        return False
-    extract_path = archive_path.split("/")[1].split(".")[0]
-    if run("rm -rf /data/web_static/releases/{}/".format(
-            extract_path)).failed is True:
-        return False
-    if run(
-            "mkdir -p /data/web_static/releases/{}".format(
-                extract_path)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
-            archive_path.split("/")[1], extract_path)).failed is True:
-        return False
-    if run("rm -rf /tmp/{}".format(archive_path.split("/")[1])).failed is True:
-        return False
-    part1 = "mv /data/web_static/releases/{}/web_static/*".format(extract_path)
-    part2 = "/data/web_static/releases/{}/".format(extract_path)
-    mv = "{} {}".format(part1, part2)
-    if run(mv).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        pass
-    if run(
-        "ln -sf /data/web_static/releases/{}/ /data/web_static/current"
-            .format(extract_path)).failed is True:
-        return False
-    return True
+    """
+        Distribute archive.
+    """
+    if os.path.exists(archive_path):
+        archived_file = archive_path[9:]
+        newest_version = "/data/web_static/releases/" + archived_file[:-4]
+        archived_file = "/tmp/" + archived_file
+        # upload file to /tmp/
+        put(archive_path, "/tmp/")
+        # create target directory
+        run("sudo mkdir -p {}".format(newest_version))
+        # uncompress folders to target_directory
+        run("sudo tar -xzf {} -C {}/".format(archived_file,
+                                             newest_version))
+        # delete archive
+        run("sudo rm {}".format(archived_file))
+        # move files from web_static to root of target folder
+        run("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
+        # delete empty web_static directory
+        run("sudo rm -rf {}/web_static".format(newest_version))
+        # delete sym link /data/web_static/current
+        run("sudo rm -rf /data/web_static/current")
+        # create new sym link between /data/web_static/releases/arch and /data/web_static/current
+        run("sudo ln -s {} /data/web_static/current".format(newest_version))
+
+        print("New version deployed!")
+        return True
+
+    return False
